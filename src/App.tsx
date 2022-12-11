cf7065e6ce86da85a7be4c571b44200072058b18
nivaldoandrade <nivaldoandradef@gmail.com>
2022-12-11 20:34:29 -0300

    creating global styles and theme / adding fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import { ThemeProvider } from 'styled-components';
+
+import theme from './styles/themes/default';
+
+import { GlobalStyles } from './styles/GlobalStyles';
+
 export function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <h1>Hello World!</h1>
+    </ThemeProvider>
+  );
 }
